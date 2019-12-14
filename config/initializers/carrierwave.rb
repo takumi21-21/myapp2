b4e3345ed571a6245dc755271024f189452b65a9
阿部拓海 <abetakumi@takumiabe.local>
2019-12-14 12:24:01 +0900

    addS3

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,11 @@
+f Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID_MER60"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY_MER60"],
+      region: 'ap-northeast-1' # S3バケット作成時に指定したリージョン。左記は東京を指す
+    }
+    config.fog_directory  = 'tukemenimage' # 作成したS3バケット名
+  end
+end
