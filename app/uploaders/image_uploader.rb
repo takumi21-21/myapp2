a0ea062f2cbb65a92c8e04cf82e5dc3026bcb15b
阿部拓海 <abetakumi@takumiabe.local>
2019-12-14 13:27:58 +0900

    imageuploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,10 +4,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-
-
-  storage :file
-　# storage :fog
+  storage :true
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
